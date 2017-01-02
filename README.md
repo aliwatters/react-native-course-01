e79c6047a448f7f322ceed8e1cb5dd0ddfa72379
Ali Watters <ali.watters@gmail.com>
2017-01-02 09:35:02 -0800

    wip - android stopwatch app, readme notes

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Following along with https://www.udemy.com/reactnative - Build Apps with React Native
 
-Note: apps in course are built with `react-native init <app name> --version 0.24.0` - used that for weekdays
+## Notes
+* in the course apps are built on a mac - I'm using linux, and therefore android
+* apps in course are built with `react-native init <app name> --version 0.24.0` - used that for weekdays
+* useful flex box cheatsheet at: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+* access `console.<blah>` by running `$ react-native log-android`
--- stopwatch/index.android.js
+++ stopwatch/index.android.js
@@ -1,35 +1,82 @@
 import React, {Component} from 'react';
 import {
-    Text,
-    View,
     AppRegistry,
-    StyleSheet
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View
 } from 'react-native';
 
 var StopWatch = React.createClass({
   render: function() {
-    return <View>
-      <Text>
-        00:00.00
-      </Text>
-      {this.startStopButton()}
-      {this.lapButton()}
+    return <View style={styles.container}>
+      <View style={[styles.header, this.border('yellow')]}>
+        <View style={[styles.timerWrapper,this.border('red')]}>
+        <Text>
+          00:00.00
+        </Text>
+        </View>
+        <View style={[styles.buttonWrapper,this.border('green')]}>
+          {this.startStopButton()}
+          {this.lapButton()}
+        </View>
+      </View>
+      <View style={[styles.footer,this.border('blue')]}>
+        <Text>
+          A list of laps.
+        </Text>
+      </View>
     </View>
   },
   startStopButton: function() {
-    return <View>
+    return <TouchableHighlight underlayColor='gray' onPress={this.handleStartPress}>
       <Text>Start</Text>
-    </View>
+    </TouchableHighlight>
   },
   lapButton:function() {
     return <View>
       <Text>Lap</Text>
     </View>
+  },
+  handleStartPress: function() {
+    console.log('Start Pressed')
+  },
+  border: function(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4
+    }
   }
 });
 
 var styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems: 'stretch'
+  },
+  header: { // Yellow
+    flex:1
+  },
+  footer: { // Blue
+    flex:1
+  },
+  timerWrapper: { // Red
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: { // Green
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  stopWrapper: {
 
+  },
+  startWrapper: {
+
+  }
 });
 
 AppRegistry.registerComponent('stopwatch', () => StopWatch);
