e3baaa984c14bba18bb71b45ba2900d7293edd4f
Ali Watters <ali.watters@gmail.com>
2017-01-05 21:24:13 -0800

    wip video 61 - local parse server settings

--- authentication/src/main.js
+++ authentication/src/main.js
@@ -10,7 +10,9 @@ import Parse from 'parse/react-native';
 
 module.exports = React.createClass({
   componentWillMount: function() {
-    Parse.initialize("APPLICATION_ID", "MASTER_KEY");
+    Parse.initialize("APPLICATION_ID");
+    Parse.serverURL = 'http://localhost:1337/parse';
+
   },
   render: function() {
     return <View style={styles.container}>
